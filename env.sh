a36fc8d718e9731989557dd1d110821187962b6e
countrymanar <33734862+countrymanar@users.noreply.github.com>
2018-04-20 13:11:41 -0500

    Update env.sh

--- env.sh
+++ env.sh
@@ -10,8 +10,8 @@ export TF_VAR_fingerprint="Enter Your User Fingerprint Here"
 export TF_VAR_region="us-ashburn-1"
 
 #Below Variables Shouldn't Be Changed if Following the OCI Workshop
-export TF_VAR_image_ocid="ocid1.image.oc1.iad.aaaaaaaautkmgjebjmwym5i6lvlpqfzlzagvg5szedggdrbp6rcjcso3e4kq"
-export TF_VAR_instance_shape="VM.Standard1.2"
-export TF_VAR_private_key_path="userdata/keys/APIkey.pem"
-export TF_VAR_ssh_public_key=$(cat userdata/keys/rsa-key-MEAN.ssh.pub)
-export TF_VAR_ssh_authorized_private_key=$(cat userdata/keys/rsa-key-MEAN.ssh)
+#export TF_VAR_image_ocid="ocid1.image.oc1.iad.aaaaaaaautkmgjebjmwym5i6lvlpqfzlzagvg5szedggdrbp6rcjcso3e4kq"
+#export TF_VAR_instance_shape="VM.Standard1.2"
+#export TF_VAR_private_key_path="userdata/keys/APIkey.pem"
+#export TF_VAR_ssh_public_key=$(cat userdata/keys/rsa-key-MEAN.ssh.pub)
+#export TF_VAR_ssh_authorized_private_key=$(cat userdata/keys/rsa-key-MEAN.ssh)
